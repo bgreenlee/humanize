8cc6150895722aa8ca33fb2db3c93b702357972d
Brad Greenlee <brad@footle.org>
2020-10-10 17:13:36 -0700

    Add option to preserve formatting

--- humanize.go
+++ humanize.go
@@ -12,17 +12,23 @@ import (
 	"strconv"
 )
 
-var version = "v1.1.1"
+var version = "v1.2.0"
+
+type options struct {
+	isBinary           bool
+	minValue           float64
+	preserveFormatting bool
+}
 
 // numToHuman returns a replacement function used by rexexp.ReplaceAllStringFunc
 // to replace numbers in a string with more human-readable values
-func numToHuman(isBinary bool, minValue float64) func(string) string {
+func numToHuman(opt options) func(string) string {
 	var suffixes = []string{"K", "M", "G", "T", "P", "E", "Z", "Y"}
 
 	var base = 10.0      // float because it reduces the number of casts we have to do below
 	var expdivisor = 3.0 // each successive suffix represents a Pow(base, expdivisor) increase
 
-	if isBinary {
+	if opt.isBinary {
 		base = 2
 		expdivisor = 10
 	}
@@ -31,7 +37,7 @@ func numToHuman(isBinary bool, minValue float64) func(string) string {
 		// we parse as a float64 because it allows us to handle much larger values than uint64
 		if num, err := strconv.ParseFloat(numstr, 64); err == nil {
 			// don't bother if it's small enough
-			if num < minValue {
+			if num < opt.minValue {
 				return numstr
 			}
 
@@ -49,11 +55,16 @@ func numToHuman(isBinary bool, minValue float64) func(string) string {
 			}
 
 			var suffix = suffixes[suffidx]
-			if isBinary {
+			if opt.isBinary {
 				suffix = suffix + "i"
 			}
 
-			return fmt.Sprintf("%d%s", shortnum, suffix)
+			if opt.preserveFormatting {
+				// left-pad with spaces
+				return fmt.Sprintf("%*d%s", len(numstr)-1, shortnum, suffix)
+			} else {
+				return fmt.Sprintf("%d%s", shortnum, suffix)
+			}
 		} else {
 			// if we failed to parse the number for whatever reason, just return the original
 			return numstr
@@ -64,13 +75,13 @@ func numToHuman(isBinary bool, minValue float64) func(string) string {
 
 // humanize takes a Reader and configuration flags and replaces any numbers it reads
 // with more human-readable versions
-func humanize(reader io.Reader, writer io.Writer, isBinary bool, minValue float64) {
+func humanize(reader io.Reader, writer io.Writer, opt options) {
 	numre := regexp.MustCompile(`\d+`)
 	bufreader := bufio.NewReader(reader)
 	//bufwriter := bufio.NewWriter(*writer)
 	for {
 		line, err := bufreader.ReadString('\n')
-		fmt.Fprint(writer, numre.ReplaceAllStringFunc(line, numToHuman(isBinary, minValue)))
+		fmt.Fprint(writer, numre.ReplaceAllStringFunc(line, numToHuman(opt)))
 		if err == io.EOF {
 			break
 		}
@@ -81,16 +92,19 @@ func humanize(reader io.Reader, writer io.Writer, isBinary bool, minValue float6
 }
 
 func main() {
-	binaryFlagPtr := flag.Bool("bin", false, "use base-2 divisors instead of base-10")
-	minValuePtr := flag.Float64("min", 1000, "minimum absolute value to humanize")
-	versionFlagPtr := flag.Bool("version", false, "print version and exit")
+	var opt options
+	flag.BoolVar(&opt.isBinary, "binary", false, "use base-2 divisors instead of base-10")
+	flag.Float64Var(&opt.minValue, "min", 1000, "minimum absolute value to humanize")
+	flag.BoolVar(&opt.preserveFormatting, "preserve", false, "preserve formatting when replacing")
+	var versionFlag bool
+	flag.BoolVar(&versionFlag, "version", false, "print version and exit")
 
 	flag.Parse()
 
-	if *versionFlagPtr {
+	if versionFlag {
 		fmt.Printf("humanize %s\n", version)
 		os.Exit(0)
 	}
 
-	humanize(os.Stdin, os.Stdout, *binaryFlagPtr, *minValuePtr)
+	humanize(os.Stdin, os.Stdout, opt)
 }
